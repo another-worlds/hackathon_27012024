cb7b6128639880d1c1073c699f317b1dadf8d723
another-worlds <a.egorshev@gmail.com>
2024-01-27 01:59:12 +0600

    Updated Milestone 1.a to a streamlit app

--- langchain_intro/main.py
+++ langchain_intro/main.py
@@ -1,4 +1,16 @@
 import langchain_helper as lch
 import streamlit as st
 
-st.title(lch.generate_pet_name("cat"))
+pet_list = ['Dog', 'Cat', 'Cow', 'Zebra', 'Parrot']
+
+st.title("Pet name generator")
+
+animal_type = st.sidebar.selectbox("What is your pet?", pet_list)
+
+pet_color = st.sidebar.text_area(
+    f"What color is your {animal_type}",
+    max_chars=15)
+
+if pet_color:
+    response = lch.generate_pet_name(animal_type, pet_color)
+    st.text(response['text'])
