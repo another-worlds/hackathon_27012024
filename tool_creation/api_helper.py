03d7342979d9a1f3a2145b0ed87fbd4bbc2ad425
another-worlds <a.egorshev@gmail.com>
2024-01-27 03:50:30 +0600

    Init branch commit

---
+++ tool_creation/api_helper.py
@@ -0,0 +1,6 @@
+import yfinance
+import pandas as pd
+import numpy as np
+
+def get_price_change(tf=1, ticker="BTC"):
+    pass
---
+++ tool_creation/langchain_helper.py
@@ -0,0 +1,5 @@
+from langchain_openai import OpenAI, OpenAIEmbeddings
+from langchain.agents import AgentType
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+
